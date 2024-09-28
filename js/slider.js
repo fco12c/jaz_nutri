ab4fdc0fb873a4f0ab35a6ccd7db3aa893aaf520
fco12c <48927830+fco12c@users.noreply.github.com>
2024-09-28 12:18:56 -0600

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,12 @@
+let sliderInner = document.querySelector(".slider--inner");
+let images = sliderInner.querySelectorAll("img");
+let index = 1;
+setInterval(function(){
+    let percentage = index * -100;
+    sliderInner.style.transform = "translateX("+ percentage +"%)";
+    index++;
+    //console.log(images.length);
+    if( index > images.length - 1 ){
+        index = 0;
+    }
+}, 3000);
