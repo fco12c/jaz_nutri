7ed75165893d894dd08a3ee67aea0c5a96e73714
fco12c <48927830+fco12c@users.noreply.github.com>
2024-10-03 14:02:19 -0600

    configuracion de loader

---
+++ loader.js
@@ -0,0 +1,8 @@
+setTimeout(function(){
+    document.getElementById("loading-page").className += "loaded";
+    document.getElementById("loader").className += "opzero";
+    document.body.className += "whitebk";
+},6500);
+setTimeout(function(){
+    document.getElementById("mainNav").style.display = "block";
+},6800);
